17ae7e9a1bb64942b79504d977c359c516f1da1e
legend524 <thecrazydev524@gmail.com>
2023-09-05 00:32:06 +0900

    finished presale design

--- src/pages/Presale.jsx
+++ src/pages/Presale.jsx
@@ -1,7 +1,107 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Line } from 'rc-progress';
 
 export default function Presale() {
+
+  const [active, setActive] = useState(0)
   return (
-    <div>Presale</div>
+    <div className="presale">
+      <div className="presale_banner">
+        PRESALE WILD BASE
+      </div>
+      <div className="presale_content">
+        <div className="card">
+          <div className="presale_card">
+            <div className="tab_box">
+              <div className={`tab_panel  + ${active === 0 ? 'active' : ''}`} onClick={() => setActive(0)}>SALE</div>
+              <div className={`tab_panel  + ${active === 1 ? 'active' : ''}`} onClick={() => setActive(1)}>CLAIM</div>
+            </div>
+            {active === 0 ? (
+              <div>
+                <div className="sale_percent_bar">
+                  <div className="text">0/100 ETH</div>
+                  <div className="text">0%</div>
+                </div>
+                <Line percent={20} strokeWidth={3} strokeColor="#2a8710" trailColor='#00162fb0' trailWidth={3} style={{ margin: '17px 0 30px 0' }} />
+                <div className="balance_form">
+                  <div className="form_text">
+                    Balance: 0 ETH
+                  </div>
+                  <div className="form_input">
+                    <input type="number" placeholder='0' />
+                  </div>
+                </div>
+                <button className='btn buy_btn'>BUY</button>
+                <div className="commit_text">
+                  <div>Your Commited</div>
+                  <div>0 ETH</div>
+                </div>
+              </div>
+            ) : (
+              <div className='claim_card'>
+                <div className="claim_list">
+                  <div className="list_item">
+                    <p>Your Commited</p>
+                    <p>0 ETH</p>
+                  </div>
+                  <div className="list_item">
+                    <p>ETH to Refund</p>
+                    <p>0 ETH</p>
+                  </div>
+                  <div className="list_item">
+                    <p>Claimable</p>
+                    <p>0 $</p>
+                  </div>
+                  <div className="list_item">
+                    <p>Earned Farming Bonuese</p>
+                    <p>0 $</p>
+                  </div>
+                </div>
+                <button className='claim_btn'>CLAIM</button>
+              </div>
+            )}
+          </div>
+        </div>
+        <div className="card">
+          <div className="presale_detail">
+            <div className="annual_price">
+              <p>Total Raised</p>
+              <h3>0 ETH</h3>
+            </div>
+            <div className="main_detail">
+              <div className="detail_title">
+                Main Details
+              </div>
+              <div className="detail_list">
+                <div className="list_item">
+                  <p>Token Sale Contract</p>
+                  <p><a href="http://localhost:3001/presale">0x2d6F...7a34</a></p>
+                </div>
+                <div className="list_item">
+                  <p>Sale Price</p>
+                  <p>0.0000000067 ETH</p>
+                </div>
+                <div className="list_item">
+                  <p>Total Token Sale</p>
+                  <p>400.000 $</p>
+                </div>
+                <div className="list_item">
+                  <p>Start Time</p>
+                  <p>TBA</p>
+                </div>
+                <div className="list_item">
+                  <p>End Time</p>
+                  <p>TBA</p>
+                </div>
+                <div className="list_item">
+                  <p>Minimum / Maximum Committed</p>
+                  <p>0.01 ETH / 1 ETH</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
