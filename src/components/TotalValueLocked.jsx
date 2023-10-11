f13785c24670cbb77f03af029f865b04b8742206
legend524 <thecrazydev524@gmail.com>
2023-10-11 12:04:05 +0900

    added liquidity and removed wildxpersecond

--- src/components/TotalValueLocked.jsx
+++ src/components/TotalValueLocked.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { useTranslation } from 'contexts/Localization'
 import { useTotalSupply, useWILDXPerSecond } from 'hooks/useTokenBalance'
-import { usePriceWILDXUsdc, useTotalValue } from 'state/hooks'
+import { usePriceWILDXUsdc, useTotalValue, usePriceEthUsdc } from 'state/hooks'
 import CardValue from './FarmStackingComponents/CardValue'
 import { Skeleton } from 'uikit'
 import { convertCurrency, toReadableAmount } from 'utils/customHelpers'
-import { useContractRead } from 'wagmi';
-import { getWILDXAddress } from 'utils/addressHelpers'
+import { useContractRead, erc20ABI } from 'wagmi';
+import { getWILDXAddress, getWethAddress } from 'utils/addressHelpers'
 import wildABI from 'config/abi/wild.json'
 
 export default function TotalValueLocked() {
@@ -16,9 +16,11 @@ export default function TotalValueLocked() {
     ? tvlData.toLocaleString('en-US', { maximumFractionDigits: 1 })
     : 0
   const wildUsdcPrice = usePriceWILDXUsdc().toNumber()
+  const wethPrice = usePriceEthUsdc().toNumber()
+
   const totalSupply = useTotalSupply()
   const wildxPerBlock = useWILDXPerSecond()
-
+  // 0xeAA13b4f85A98E6CcaF65606361BD590e98DE2Cb
   const tokenABalanceRead = useContractRead({
     address: getWILDXAddress(),
     abi: wildABI,
@@ -26,8 +28,24 @@ export default function TotalValueLocked() {
     args: ['0x000000000000000000000000000000000000dead'],
     chainId: 8453
   })
+  const wildxBalanceRead = useContractRead({
+    address: getWILDXAddress(),
+    abi: wildABI,
+    functionName: 'balanceOf',
+    args: ['0xeAA13b4f85A98E6CcaF65606361BD590e98DE2Cb'],
+    chainId: 8453
+  })
+  const wethBalanceRead = useContractRead({
+    address: getWethAddress(),
+    abi: erc20ABI,
+    functionName: 'balanceOf',
+    args: ['0xeAA13b4f85A98E6CcaF65606361BD590e98DE2Cb'],
+    chainId: 8453
+  })
+
 
   const totalMinted = totalSupply - toReadableAmount(tokenABalanceRead?.data, 18)
+  const liquidity = toReadableAmount(wildxBalanceRead?.data, 18) * wildUsdcPrice * 0.98 + toReadableAmount(wethBalanceRead?.data, 18) * wethPrice
   const marketCap = totalMinted * wildUsdcPrice
   return (
     <div className='flex-1 main_bg p-8 rounded-md'>
@@ -44,6 +62,18 @@ export default function TotalValueLocked() {
       <div className='text-3xl text-right mb-5'>WILDX Stats</div>
       <div className='flex flex-col gap-4'>
         <div className='flex items-center justify-between'>
+          <p>Liquidity</p>
+          <div className='h-30 '>
+            {liquidity > 0 ? (
+              <span className='text-[20px] font-semibold'>
+                $ {convertCurrency(liquidity)}
+              </span>
+            ) : (
+              <Skeleton width={80} height={30} />
+            )}
+          </div>
+        </div>
+        <div className='flex items-center justify-between'>
           <p>Market cap</p>
           <div className='h-30 '>
             {marketCap > 0 ? (
@@ -52,19 +82,6 @@ export default function TotalValueLocked() {
               </span>
             ) : (
               <Skeleton width={80} height={30} />
-            )}
-          </div>
-        </div>
-        <div className='flex items-center justify-between'>
-          <p>Circulating Supply</p>
-          <div>
-            {totalSupply && (
-              <CardValue
-                fontSize='20px'
-                value={totalMinted}
-                decimals={1}
-                color='#fffff1'
-              />
             )}
           </div>
         </div>
@@ -82,28 +99,16 @@ export default function TotalValueLocked() {
           </div>
         </div>
         <div className='flex items-center justify-between'>
-          <p>Total Burned</p>
+          <p>Circulating Supply</p>
           <div>
-            {totalMinted && (
+            {totalSupply && (
               <CardValue
                 fontSize='20px'
+                value={totalMinted}
                 decimals={1}
-                value={Number(toReadableAmount(tokenABalanceRead?.data, 18)) - Number(500000)}
                 color='#fffff1'
               />
             )}
-          </div>
-        </div>
-        <div className='flex items-center justify-between'>
-          <p>New WILDX/second</p>
-          <div>
-            {' '}
-            <CardValue
-              fontSize='20px'
-              decimals={5}
-              value={wildxPerBlock}
-              color='#fffff1'
-            />
           </div>
         </div>
       </div>
